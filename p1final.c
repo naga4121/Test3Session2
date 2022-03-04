ee6594dc64bdf2ca14dfe82f468911ca0cc14f4c
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2022-03-04 09:16:35 +0000

    done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
+void input(int*num1, int*den1, int*num2, int*den2)
+{
+  printf("Enter the num1:");
+  scanf("%d",num1);
+  printf("Enter the den1:");
+  scanf("%d",den1);
+  printf("Enter the num2:");
+  scanf("%d",num2);
+  printf("Enter the den2:");
+  scanf("%d",den2);
+}
+void add(int num1,int den1,int num2,int den2,int*num3,int*den3)
+{
+int gcd, c, x, y;
+  x=(num1*den2)+(num2*den1);
+  y=(den1*den2);
+  for(c=1; c<=x && c<=y; ++c)
+    {
+       if(x%c==0 && y%c==0)
+          gcd=c;
+    }
+  *num3=x/gcd;
+  *den3=y/gcd;
+}
+void output(int num1,int den1,int num2,int den2,int num3,int den3)
+{
+  printf("%d/%d + %d/%d = %d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
+}
